61dfe9ae86907bc3d1a7909a7676a4477b27be05
SuperPyDroCh <ivashka.pri@ya.ru>
2024-11-06 00:53:02 +0300

    big commit

--- LCD_effects/LCD_effects.ino
+++ LCD_effects/LCD_effects.ino
@@ -1,27 +1,55 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
+const unsigned short tmr1 = 500;    //Cursor flicker rate, ms
+const unsigned short tmr2 = 200;    //Character output rate, ms
+unsigned long PassedTime = 0;
+
+String ToWrite;                     //Queue of output
+unsigned char CursorX, CursorY;
+bool CursorF;
+unsigned short for_i = 0;           //Variable for serial output
+
+bool FO0 = false;                   //For Once #0
+bool FO1 = false;                   //For Once #1
 
 void setup(){
   lcd.init();
   lcd.backlight();
-  coolWrite("abcdefg");
 }
 
 void loop(){
+  /*###some code################
+  ############################*/
+  if (!FO0){CursorX = 1; CursorY = 1; FO0 = true;}
+  if (!FO1){ToWrite = "Hi!"; FO1 = true;}
+  /*###some code################
+  ############################*/
+  
+  //-------мигание курсором----------//
+  if (millis() - PassedTime >= tmr1){
+    lcd.setCursor(CursorX, CursorY);
+    PassedTime = millis();
+    if(CursorF){
+      lcd.write(255);
+    } else {
+      lcd.print(" ");
+    }
+    CursorF = !CursorF;
+  }
 
-}
-
-
-void coolWrite(String str){
-  String printedStr;
-  for (int c = 0; c < str.length(); c++){
-    printedStr += str[c];
-    lcd.print(printedStr);
-    lcd.write(255);
-    delay(300);
-    if (c != str.length()-1){
-      lcd.clear();
+  if (ToWrite != ""){
+    if (millis() - PassedTime >= tmr2){
+      if (for_i < ToWrite.length()){
+        lcd.setCursor(CursorX, CursorY);
+        PassedTime = millis();
+        CursorX++;
+        lcd.print(ToWrite[for_i]);
+        for_i++;
+      } else {
+        ToWrite = "";
+        for_i = 0;
+      }
     }
   }
 }
